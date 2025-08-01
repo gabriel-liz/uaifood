a63ea3f1628551175fa884e0ff4f25cecdfe66b1
Gabriel <gabriel.liz@outlook.com.br>
2025-08-01 16:20:35 -0300

    Implementado entidade e repositorio de Restaurante

---
+++ src/main/java/com/uaifood/domain/repository/RestauranteRepository.java
@@ -0,0 +1,14 @@
+package com.uaifood.domain.repository;
+
+import java.util.List;
+
+import com.uaifood.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+	
+	List<Restaurante> todas();
+	Restaurante porId(Long id);
+	Restaurante adicionar(Restaurante restaurante);
+	void remover(Restaurante restaurante);
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('Niu Sushi', 20);
+insert into restaurante (nome, taxa_frete) values ('Mac', 10);
