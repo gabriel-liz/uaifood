a1a37d9c7d7e43e7da176f008f4e9a39036a808d
Gabriel <gabriel.liz@outlook.com.br>
2025-08-01 12:44:10 -0300

    Adicionada instancia de banco de dados

---
+++ src/main/java/com/uaifood/UaifoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.uaifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaifoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaifoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uaifood/UaifoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uaifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaifoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
