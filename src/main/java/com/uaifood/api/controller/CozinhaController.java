a4b48632490f1944e35e5123faa91ab19c4938bc
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-04 17:16:20 -0300

    Aqui foram implementadas formas de manipular a resposta HTTP com ResponseEntity

--- src/main/java/com/uaifood/api/controller/CozinhaController.java
+++ src/main/java/com/uaifood/api/controller/CozinhaController.java
@@ -1,9 +1,13 @@
 package com.uaifood.api.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +37,25 @@ public class CozinhaController {
 	}	
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {		
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		//Dessa forma nao retorna o corpo com o resultado
+		
+		//return ResponseEntity.status(HttpStatus.OK).build();
+		//geralmente o response entity dessa forma eh utilizado quando podem haver outras formas de resposta dependendo de um iF
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		
+		//Aqui uma forma caso a url/end-point esteja indiposnivel, podemos indicar qual é o que esta sendo usado temporariamente
+//		
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.add(HttpHeaders.LOCATION, "http://url-temporario.uaifood.com/cozinhas");
+//		return ResponseEntity
+//				.status(HttpStatus.FOUND)
+//				.headers(headers)
+//				.build();
+	
+		//Aqui apenas o retorno padrao
+		return ResponseEntity.ok(cozinha);
 	}
 }
