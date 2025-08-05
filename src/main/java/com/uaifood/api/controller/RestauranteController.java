65bac5d38c72cd88314feef1ec2547c12e9d1d3a
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-05 16:13:03 -0300

    Implementado cadastro de restaurante

--- src/main/java/com/uaifood/api/controller/RestauranteController.java
+++ src/main/java/com/uaifood/api/controller/RestauranteController.java
@@ -3,14 +3,19 @@ package com.uaifood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uaifood.domain.model.Restaurante;
 import com.uaifood.domain.repository.RestauranteRepository;
+import com.uaifood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -18,6 +23,9 @@ public class RestauranteController {
 	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
 	
 	@GetMapping
 	public List<Restaurante> listar(){
@@ -32,6 +40,11 @@ public class RestauranteController {
 			return ResponseEntity.ok(restaurante);
 		}
 		return ResponseEntity.notFound().build();
-	}	
+	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Restaurante adicionar(@RequestBody Restaurante restaurante){
+		return cadastroRestaurante.salvar(restaurante);
+	}	
 }
--- src/main/java/com/uaifood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/uaifood/domain/service/CadastroRestauranteService.java
@@ -1,5 +1,20 @@
 package com.uaifood.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uaifood.domain.model.Restaurante;
+import com.uaifood.domain.repository.RestauranteRepository;
+
+@Service
 public class CadastroRestauranteService {
+	
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		return restauranteRepository.salvar(restaurante);
+			
+	}
 
 }
