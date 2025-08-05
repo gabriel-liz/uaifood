58dd62ea608ec61aea2f15caf4e0ed4b98c12d4b
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-05 11:33:39 -0300

    Criada camada de serviço para que operações de banco não sejam realizadas diratamente pelo controller

--- src/main/java/com/uaifood/api/controller/CozinhaController.java
+++ src/main/java/com/uaifood/api/controller/CozinhaController.java
@@ -2,12 +2,10 @@ package com.uaifood.api.controller;
 
 
 import java.util.List;
-import com.uaifood.infrastructure.repository.CozinhaRepositoryImpl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.uaifood.api.model.CozinhasXmlWrapper;
 import com.uaifood.domain.model.Cozinha;
 import com.uaifood.domain.repository.CozinhaRepository;
@@ -30,8 +27,7 @@ import com.uaifood.domain.service.CadastroCozinhaService;
 @RestController
 @RequestMapping(value = "/cozinhas" , produces = MediaType.APPLICATION_JSON_VALUE) 
 public class CozinhaController {
-
-    private final CozinhaRepositoryImpl cozinhaRepositoryImpl;
+    
     
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
@@ -39,11 +35,7 @@ public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-
-    CozinhaController(CozinhaRepositoryImpl cozinhaRepositoryImpl) {
-        this.cozinhaRepositoryImpl = cozinhaRepositoryImpl;
-    }	
-	
+  
 	@GetMapping
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
