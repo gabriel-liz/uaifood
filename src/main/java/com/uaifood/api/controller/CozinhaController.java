cdff23cd3e6e672be805a73c5f850d1634322430
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-05 14:12:57 -0300

    Criada camada de serviço para exclusao de cozinha para que operações de banco não sejam rezalidas diretamente pelo controller. Tambem feitas validação/retornos de exception pra quando a entidade excluida possuir algum vinculo e para quando ela não existir

--- src/main/java/com/uaifood/api/controller/CozinhaController.java
+++ src/main/java/com/uaifood/api/controller/CozinhaController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.uaifood.api.model.CozinhasXmlWrapper;
+import com.uaifood.domain.exception.EntidadeEmUsoException;
+import com.uaifood.domain.exception.EntidadeNaoEncontradaException;
 import com.uaifood.domain.model.Cozinha;
 import com.uaifood.domain.repository.CozinhaRepository;
 import com.uaifood.domain.service.CadastroCozinhaService;
@@ -78,14 +80,13 @@ public class CozinhaController {
 	@DeleteMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId){
 		try {
-			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+			cadastroCozinha.excluir(cozinhaId);
+			return ResponseEntity.noContent().build();
 			
-			if(cozinha != null) {
-				cozinhaRepository.remover(cozinha);
-				return ResponseEntity.noContent().build();
-			}		
-			return ResponseEntity.notFound().build();				
-		}catch(DataIntegrityViolationException e) {
+		}catch (EntidadeNaoEncontradaException e){
+			return ResponseEntity.notFound().build();	
+			
+		}catch(EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
---
+++ src/main/java/com/uaifood/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,15 @@
+package com.uaifood.domain.exception;
+
+public class EntidadeEmUsoException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EntidadeEmUsoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	
+}
