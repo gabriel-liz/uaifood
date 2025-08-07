1ebbe8eade45986664284a848a6689a7caedbfe1
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-07 15:03:34 -0300

    Implementado pesquisar por nome restaurante e por idCozinha, direto na interface

--- src/main/java/com/uaifood/api/controller/TesteController.java
+++ src/main/java/com/uaifood/api/controller/TesteController.java
@@ -1,5 +1,6 @@
 package com.uaifood.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uaifood.domain.model.Cozinha;
+import com.uaifood.domain.model.Restaurante;
 import com.uaifood.domain.repository.CozinhaRepository;
+import com.uaifood.domain.repository.RestauranteRepository;
 
 @RestController
 @RequestMapping("/teste")
@@ -18,10 +21,23 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
 	@GetMapping("/cozinhas/por-nome")
 	public List<Cozinha> cozihasPorNome(@RequestParam String nome){
 		//return cozinhaRepository.findByNome(nome);
 		return cozinhaRepository.findByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/por-taxa-frete")
+	public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial,BigDecimal taxaFinal){
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+	
+	@GetMapping("/restaurantes/por-nome")
+	public List<Restaurante> restaurantesPorTaxaFrete(String nome, Long cozinhaId){
+		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
 	}
 
 }
--- src/main/java/com/uaifood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/uaifood/domain/repository/RestauranteRepository.java
@@ -1,5 +1,6 @@
 package com.uaifood.domain.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import com.uaifood.domain.model.Restaurante;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
+	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 }
