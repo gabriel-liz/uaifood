9bc2b841556d864069c38aaba9a3e0671d3e84c9
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-07 10:49:57 -0300

    Feita buscar por nome da Entidade Cozinha utilizando JPQL

--- src/main/java/com/uaifood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/uaifood/domain/repository/CozinhaRepository.java
@@ -7,7 +7,8 @@ import com.uaifood.domain.model.Cozinha;
 public interface CozinhaRepository {
 	
 	List<Cozinha> listar();
-	Cozinha buscar(Long id);
+	List<Cozinha> consultarPorNome(String nome);
+	Cozinha buscar(Long id);	
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
 }
--- src/main/java/com/uaifood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/uaifood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -7,7 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.uaifood.domain.model.Cozinha;
@@ -15,7 +15,7 @@ import com.uaifood.domain.repository.CozinhaRepository;
 
 
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository{
 
 	@PersistenceContext
@@ -51,6 +51,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 		}
 		
 		manager.remove(cozinha);
+	}
+
+	@Override
+	public List<Cozinha> consultarPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();		
 	}
 	
 }
