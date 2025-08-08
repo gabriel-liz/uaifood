515542e112c8467a7fce3f147863b5b1bb6638ff
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-08 16:32:11 -0300

    Refatorado consulta de restaurante por nome e frete para utilizar a API Criteria, nesse commit foi feita apenas uma consulta simples

--- src/main/java/com/uaifood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/uaifood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -22,33 +24,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@Override
 	public List<Restaurante> find(String nome, 
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {		
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		var parametros = new HashMap<String, Object>();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class); //from Restaurante		
 		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		
-		if (taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-		
-		if (taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
-		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
 		return query.getResultList();
 	}
 }
