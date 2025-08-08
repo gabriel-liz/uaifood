c45669d886133914edc6d6856fff6c8ed098fff6
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-08 16:02:44 -0300

    Implementado repositorio SDJ customizado para restaurante

--- src/main/java/com/uaifood/api/controller/TesteController.java
+++ src/main/java/com/uaifood/api/controller/TesteController.java
@@ -66,4 +66,10 @@ public class TesteController {
 		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}	
 	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantePorNomeFrete(String nome,
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}
+	
 }
--- src/main/java/com/uaifood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/uaifood/domain/repository/RestauranteRepository.java
@@ -5,27 +5,30 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.uaifood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-	//Tambem funciona assim
-	//List<Restaurante> queryQualquerCoisaByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-	
-	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	// Tambem funciona assim
+	// List<Restaurante> queryQualquerCoisaByTaxaFreteBetween(BigDecimal
+	// taxaInicial, BigDecimal taxaFinal);
+
+	// @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
-	//  O metodo acima faz as mesmas coisas que o metodo comentado abaixo, porem sem expor o nome dos atributos. Para testar apenas mudar a chamado na classe TesteController
-	//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
-	
+	// O metodo acima faz as mesmas coisas que o metodo comentado abaixo, porem sem
+	// expor o nome dos atributos. Para testar apenas mudar a chamado na classe
+	// TesteController
+	// List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long
+	// cozinhaId);
+
 	Optional<Restaurante> findFirstByNomeContaining(String nome);
-	
+
 	List<Restaurante> findTop2ByNomeContaining(String nome);
-	
+
 	int countByCozinhaId(Long cozinha);
 }
