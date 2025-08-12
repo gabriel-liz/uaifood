4d33373c7ce8aac6c9102886ba5ee435fd9538a1
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-12 10:49:23 -0300

    Mapeamento relacionamento bidirecional com @OneToMany, nesse commit a ligacao entre as entidades não se reflete no banco, mas é possivel aplicar alguma regra de negocio

--- src/main/java/com/uaifood/domain/model/Restaurante.java
+++ src/main/java/com/uaifood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -24,11 +25,11 @@ public class Restaurante {
 	
 	private String nome;
 	
-	 @Column(nullable = false)
+	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
-	//@JoinColumn(name= "cozinha_id") em algum caso que o nome da coluna no banco seja diferente, fazemos assim pra relacioanar a coluna do codigo com a coluna do banco.
+	@JoinColumn(name= "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
 	
