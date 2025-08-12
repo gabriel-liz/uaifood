485647320f09ce287785678a53247ffb326232c1
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-12 16:25:10 -0300

    Feito o mapeamento de outras entidades com relacionamentos muitos-para-um, um-para-muitos e muitos-para-muitos

--- src/main/java/com/uaifood/api/controller/RestauranteController.java
+++ src/main/java/com/uaifood/api/controller/RestauranteController.java
@@ -70,7 +70,8 @@ public class RestauranteController {
 		try {
 			Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
 			if (restauranteAtual.isPresent()) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco");
+				BeanUtils.copyProperties(restaurante, restauranteAtual.get(),
+						"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 
 				Restaurante restalranteSalvo = cadastroRestaurante.salvar(restauranteAtual.get());
 				return ResponseEntity.ok(restalranteSalvo);
