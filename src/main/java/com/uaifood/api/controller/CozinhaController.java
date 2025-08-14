a3f34ff2ceaee398eaa2e7ba1c3c7191aefbcb69
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-14 12:51:59 -0300

    Lancando exceções customizadas para cozinha anotadas com @ResponseStatus

--- src/main/java/com/uaifood/api/controller/CozinhaController.java
+++ src/main/java/com/uaifood/api/controller/CozinhaController.java
@@ -67,17 +67,23 @@ public class CozinhaController {
 	}
 
 	@DeleteMapping("/{cozinhaId}")
-	public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
-		try {
-			cadastroCozinha.excluir(cozinhaId);
-			return ResponseEntity.noContent().build();
-
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND)// notFound().build();
-					.body(e.getMessage());
-
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-		}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId) {		
+			cadastroCozinha.excluir(cozinhaId);			
 	}
+	
+//	@DeleteMapping("/{cozinhaId}")
+//	public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
+//		try {
+//			cadastroCozinha.excluir(cozinhaId);
+//			return ResponseEntity.noContent().build();
+//
+//		} catch (EntidadeNaoEncontradaException e) {
+//			return ResponseEntity.status(HttpStatus.NOT_FOUND)// notFound().build();
+//					.body(e.getMessage());
+//
+//		} catch (EntidadeEmUsoException e) {
+//			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+//		}
+//	}
 }
