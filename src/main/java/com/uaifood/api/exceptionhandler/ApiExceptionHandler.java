c003b67be985a028e5845369373b291a819f7509
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-15 11:10:26 -0300

    Criada uma exception Handler global, para que as exceções de todos os outros controladores, de todo o projeto, sejam tratadas. Notações @ExceptionHandler e @ControllerAdvice

---
+++ src/main/java/com/uaifood/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.uaifood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.uaifood.domain.exception.EntidadeNaoEncontradaException;
+import com.uaifood.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
+			EntidadeNaoEncontradaException e){
+		
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(
+			NegocioException e){
+		
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarMediaTypeNotSupportedException(){
+		//caso seja informado um xml ao invez de um json
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem("O tipo de mídia não é aceito").build();
+		
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+				.body(problema);
+	}
+
+}
