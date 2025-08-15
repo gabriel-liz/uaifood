4c8f21aecde9d5c43bfb25e92942edc1d39c8d11
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-15 10:54:09 -0300

    Tratando excessoes em nivel de controlador com @ExceptionHandler, para tratar excessoes que acontecem dentro do controlador. Por ter a causa da Exception(segundo parametro, o 'e', mesmo que seja uma exception mais acima, caso tenha a causa, ele passa para exception filha caso a causa seja compativel)

--- src/main/java/com/uaifood/api/controller/CidadeController.java
+++ src/main/java/com/uaifood/api/controller/CidadeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.uaifood.domain.exception.EntidadeNaoEncontradaException;
 import com.uaifood.domain.exception.EstadoNaoEncontradoException;
 import com.uaifood.domain.exception.NegocioException;
 import com.uaifood.domain.model.Cidade;
@@ -65,6 +68,20 @@ public class CidadeController {
 	@DeleteMapping("/{cidadeId}")
 	public void remover(@PathVariable Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
+	}
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
+			EntidadeNaoEncontradaException e){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(e.getMessage());
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(
+			NegocioException e){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(e.getMessage());
 	}
 
 }
