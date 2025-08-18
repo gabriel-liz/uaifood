5f3b27bb640fca30e2e557e3abb08e9e369c314a
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-18 15:18:20 -0300

    Convertendo grupos de constraints para validação em cascate com @ConvertGroup, gerando menos codigo que o commit anterior.

--- src/main/java/com/uaifood/domain/model/Restaurante.java
+++ src/main/java/com/uaifood/domain/model/Restaurante.java
@@ -20,6 +20,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -40,19 +42,19 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank(groups = Groups.CadastroRestaurante.class)
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
 	// @DecimalMin("0")
-	@PositiveOrZero(groups = Groups.CadastroRestaurante.class)
+	@PositiveOrZero
 	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 
-	@Valid // Diz para o beanValidation validar tambem as propriedades de cozinha(Em
-			// cascata)
-	@NotNull(groups = Groups.CadastroRestaurante.class)
-	@ManyToOne // (fetch = FetchType.LAZY)
+	@Valid
+	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
+	@NotNull
+	@ManyToOne 
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 
