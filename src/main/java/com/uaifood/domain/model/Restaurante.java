f6f3ad9c6b4487f972c871ade35f7d05dea87f62
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-20 10:08:58 -0300

    Crianda constraint de validação customizadas usando composição. Para que a configuracao a notacao seja executada/retornada é necessario comentar o PositiveOrZero={0} em messages.properties, pois sempre se da prioridade para o rotorno da notacao padrao

--- src/main/java/com/uaifood/domain/model/Restaurante.java
+++ src/main/java/com/uaifood/domain/model/Restaurante.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -28,6 +27,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.uaifood.core.validation.Groups;
+import com.uaifood.core.validation.TaxaFrete;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -48,7 +48,8 @@ public class Restaurante {
 
 	// @DecimalMin("0")
 	@NotNull 
-	@PositiveOrZero//(message = "{TaxaFrete.invalida}")
+	//@PositiveOrZero
+	@TaxaFrete
 	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 
