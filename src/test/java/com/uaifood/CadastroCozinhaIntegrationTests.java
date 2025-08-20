c7c242e2717e6bcddb559095bd6f72a6ee4ae798
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-20 15:11:39 -0300

    Criado outro teste de intragração, validando o retorno esperado para cozinha sem nome

--- src/test/java/com/uaifood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/uaifood/CadastroCozinhaIntegrationTests.java
@@ -3,6 +3,8 @@ package com.uaifood;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.validation.ConstraintViolationException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +24,24 @@ public class CadastroCozinhaIntegrationTests {
 		// cenário
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
-
 		// ação
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-
 		// validação
 		assertThat(novaCozinha).isNotNull();
 		assertThat(novaCozinha.getId()).isNotNull();
 	}
-
+	
+	@Test
+	public void testarCadastroCozinhaSemNome() {
+	   Cozinha novaCozinha = new Cozinha();
+	   novaCozinha.setNome(null);
+	   
+	   ConstraintViolationException erroEsperado =
+	      Assertions.assertThrows(ConstraintViolationException.class, () -> {
+	         cadastroCozinha.salvar(novaCozinha);
+	      });
+	   
+	   assertThat(erroEsperado).isNotNull();
+	}
+	
 }
