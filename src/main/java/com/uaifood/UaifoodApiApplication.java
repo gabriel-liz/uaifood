512c6b22fb5eaeb6f16d60c87bb8b1ef2e26f5d0
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-21 19:35:14 -0300

    Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/uaifood/UaifoodApiApplication.java
+++ src/main/java/com/uaifood/UaifoodApiApplication.java
@@ -1,5 +1,7 @@
 package com.uaifood;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +13,7 @@ import com.uaifood.infrastructure.repository.CustomJpaRepositoryImpl;
 public class UaifoodApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(UaifoodApiApplication.class, args);
 	}
 
