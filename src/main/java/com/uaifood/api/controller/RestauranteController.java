be8706b8ba091765c0cd0fee61d77711466446ef
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-21 20:40:38 -0300

    Criando DTOs para entrada de dados na API

--- src/main/java/com/uaifood/api/controller/RestauranteController.java
+++ src/main/java/com/uaifood/api/controller/RestauranteController.java
@@ -1,25 +1,15 @@
 package com.uaifood.api.controller;
 
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,13 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uaifood.api.model.CozinhaDTO;
 import com.uaifood.api.model.RestauranteDTO;
+import com.uaifood.api.model.input.RestauranteInputDTO;
 import com.uaifood.domain.exception.CozinhaNaoEncontradaException;
 import com.uaifood.domain.exception.NegocioException;
-import com.uaifood.domain.exception.ValidacaoException;
+import com.uaifood.domain.model.Cozinha;
 import com.uaifood.domain.model.Restaurante;
 import com.uaifood.domain.repository.RestauranteRepository;
 import com.uaifood.domain.service.CadastroRestauranteService;
@@ -47,10 +36,7 @@ public class RestauranteController {
 	private RestauranteRepository restauranteRepository;
 
 	@Autowired
-	private CadastroRestauranteService cadastroRestaurante;
-	
-	@Autowired
-	private SmartValidator validator;
+	private CadastroRestauranteService cadastroRestaurante;	
 
 	@GetMapping
 	public List<RestauranteDTO> listar() {
@@ -68,8 +54,11 @@ public class RestauranteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public RestauranteDTO adicionar(@RequestBody @Valid Restaurante restaurante) {
+	public RestauranteDTO adicionar(@RequestBody @Valid RestauranteInputDTO restauranteInputDTO) {
 		try {
+			
+			Restaurante restaurante = toDomainObject(restauranteInputDTO);
+			
 			return toDTO(cadastroRestaurante.salvar(restaurante));
 		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
@@ -77,8 +66,9 @@ public class RestauranteController {
 	}
 
 	@PutMapping("/{restauranteId}")
-	public RestauranteDTO atualizar(@PathVariable Long restauranteId, @RequestBody @Valid Restaurante restaurante) {
+	public RestauranteDTO atualizar(@PathVariable Long restauranteId, @RequestBody @Valid RestauranteInputDTO restauranteInputDTO) {
 		try {
+			Restaurante restaurante = toDomainObject(restauranteInputDTO);
 			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 			BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro",
 					"produtos");
@@ -91,59 +81,6 @@ public class RestauranteController {
 	@DeleteMapping("/{restauranteId}")
 	public void remover(@PathVariable Long restauranteId) {
 		cadastroRestaurante.excluir(restauranteId);
-	}
-
-//	@PatchMapping("/{restauranteId}")
-//	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
-//			HttpServletRequest request) {
-//
-//		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
-//		
-//		merge(campos, restauranteAtual, request);
-//		validate(restauranteAtual, "restaurante");
-//		return atualizar(restauranteId, restauranteAtual);
-//	}
-
-	private void validate(Restaurante restaurante, String objecName) {
-		
-		BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(restaurante, objecName);
-		
-		validator.validate(restaurante, bindingResult);
-		
-		if(bindingResult.hasErrors()) {
-			throw new ValidacaoException(bindingResult);
-		}
-		
-	}
-
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
-
-		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
-
-		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
-			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-
-			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-
-			System.out.println(restauranteOrigem);
-
-			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-				field.setAccessible(true);
-
-				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-				System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
-
-				ReflectionUtils.setField(field, restauranteDestino, novoValor);
-
-			});
-		} catch (IllegalArgumentException e) {
-			Throwable rootCause = ExceptionUtils.getRootCause(e);
-			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
-		}
-
 	}
 	
 	private RestauranteDTO toDTO(Restaurante restaurante) {
@@ -163,6 +100,20 @@ public class RestauranteController {
 		return restaurantes.stream()
 				.map(restaurante -> toDTO(restaurante) )
 				.collect(Collectors.toList());
+	}
+	
+	private Restaurante toDomainObject(RestauranteInputDTO restauranteInputDTO) {
+		Restaurante restaurante = new Restaurante();
+		restaurante.setNome(restauranteInputDTO.getNome());
+		restaurante.setTaxaFrete(restauranteInputDTO.getTaxaFrete());
+		
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(restauranteInputDTO.getCozinha().getId());
+		
+		restaurante.setCozinha(cozinha);
+		
+		return restaurante;
+		
 	}
 
 }
