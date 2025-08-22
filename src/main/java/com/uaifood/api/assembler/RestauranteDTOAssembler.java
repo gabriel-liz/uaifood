7d3dc420812b775f95ba22c8e53ca4c5c10de74a
gabriel-liz <gabriel.liz@outlook.com.br>
2025-08-21 21:04:04 -0300

    Adicionando e usando o ModelMapper. Foi criada uma classe de configuracao de ModelMapper para setar ele como uma Bean, para fazer parte do Spring. O ModelMapper serve para diminuir a quantidade de codigo repetitivo.

--- src/main/java/com/uaifood/api/assembler/RestauranteDTOAssembler.java
+++ src/main/java/com/uaifood/api/assembler/RestauranteDTOAssembler.java
@@ -3,26 +3,23 @@ package com.uaifood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.uaifood.api.model.CozinhaDTO;
 import com.uaifood.api.model.RestauranteDTO;
 import com.uaifood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDTOAssembler {
 //Classe que transforma de Entidade Restaurante para RestauranteDTO
+	
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public RestauranteDTO toDTO(Restaurante restaurante) {
-		CozinhaDTO cozinhaDTO = new CozinhaDTO();
-		cozinhaDTO.setId(restaurante.getCozinha().getId());
-		cozinhaDTO.setNome(restaurante.getCozinha().getNome());		
-		
-		RestauranteDTO restauranteDTO = new RestauranteDTO();
-		restauranteDTO.setId(restaurante.getId());
-		restauranteDTO.setNome(restaurante.getNome());
-		restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteDTO.setCozinha(cozinhaDTO);
-		return restauranteDTO;
+		return modelMapper.map(restaurante, RestauranteDTO.class);
 	}
 	
 	public List<RestauranteDTO> toCollectionDTO(List<Restaurante> restaurantes){
--- src/main/java/com/uaifood/api/assembler/RestauranteInputDTODisassembler.java
+++ src/main/java/com/uaifood/api/assembler/RestauranteInputDTODisassembler.java
@@ -1,25 +1,21 @@
 package com.uaifood.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.uaifood.api.model.input.RestauranteInputDTO;
-import com.uaifood.domain.model.Cozinha;
 import com.uaifood.domain.model.Restaurante;
 
 @Component
 public class RestauranteInputDTODisassembler {
 //Desmonta o RestauranteDTO e monta um Restaurante
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public Restaurante toDomainObject(RestauranteInputDTO restauranteInputDTO) {
-		Restaurante restaurante = new Restaurante();
-		restaurante.setNome(restauranteInputDTO.getNome());
-		restaurante.setTaxaFrete(restauranteInputDTO.getTaxaFrete());
-
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInputDTO.getCozinha().getId());
-
-		restaurante.setCozinha(cozinha);
-
-		return restaurante;
+		return modelMapper.map(restauranteInputDTO, Restaurante.class);
 
 	}
 
